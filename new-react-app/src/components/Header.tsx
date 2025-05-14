847d248dd43e321f1aa5a22011ed37f70b64d4aa
Brandonflex <brandonflex108@gmail.com>
2025-05-14 05:17:20 +0300

    rebuild

---
+++ new-react-app/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ padding: '10px', backgroundColor: '#282c34', color: 'white' }}>
+      <h1>Welcome to My React App</h1>
+    </header>
+  );
+};
+
+export default Header;
